7aa2b9965eba6fbc699623b4194142b040a9a973
DESKTOP-4QV1HO9\jason <jasongill2000@hotmail.com>
2022-03-19 14:37:44 -0400

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const App: React.FC = () => {
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
-        <span className="heading">Taskify</span>
+        <span className="heading">Task Manager</span>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
         <TodoList todos={todos} setTodos={setTodos} completedTodos={completedTodos} setCompletedTodos={setCompletedTodos} />
       </div>
